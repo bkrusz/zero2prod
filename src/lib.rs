c1aa788a05e3237e9a90b63a195063f5fd19e4aa
Bailey Kruszewski <bkrusz@icloud.com>
2024-02-13 21:46:11 -0700

    Formatting

--- src/lib.rs
+++ src/lib.rs
@@ -5,12 +5,9 @@ async fn health_check() -> HttpResponse {
 }
 
 pub fn run() -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-    })
-    .bind("127.0.0.1:8000")?
-    .run();
+    let server = HttpServer::new(|| App::new().route("/health_check", web::get().to(health_check)))
+        .bind("127.0.0.1:8000")?
+        .run();
 
     Ok(server)
 }
